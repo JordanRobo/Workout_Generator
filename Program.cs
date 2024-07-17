45839276f49aaf02d108da4073fb6a76b6341fac
Jordan Robinson <jordan@stateot.art>
2024-07-17 16:20:45 +1000

    Feat: Completed all the classes and methods to generate workout.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Program
 {
@@ -12,10 +13,13 @@ namespace Program
                 return true;
             }
 
-            if (TimeCheck() == true)
+            if (TimeCheck())
             {
-                RandomGenerator.SelectWorkoutSchema();
-            } else
+                string workout = RandomGenerator.GenerateWorkout();
+                Console.WriteLine(workout);
+                // Add in Email.SendEmail(workout); method here once ready
+            } 
+            else
             {
                 Console.WriteLine("The Time is not 5am AEST");
             }
@@ -25,39 +29,74 @@ namespace Program
 
     public class RandomGenerator
     {        
-        public static string SelectWorkoutSchema()
+        public static Schema SelectWorkoutSchema()
         {
-            string selectedSchema = "Workout Schema";
+            WorkoutData data = Workout.ReadData();
+            Random random = new Random();
+            int index = random.Next(data.Schemas.Count);
 
-            return selectedSchema;
+            return data.Schemas[index];
         }
 
-        public static int DetermineTimeLimit()
+        public static string DetermineTimeLimit()
         {
-            int timeLimit = 10;
+            Random random = new Random();
+            int[] timeValues = [ 10, 12, 15, 18, 20, 21 ];
+            int index = random.Next(timeValues.Length);
+            string timeLimit = timeValues[index].ToString();
 
             return timeLimit;
         }
 
-        public static int DetermineReps()
+        public static string DetermineReps()
         {
-            int reps = 10;
+            Random random = new Random();
+            int[] repValues = [ 5, 6, 10, 12, 15, 18, 20, 21, 25, 30 ];
+            int index = random.Next(repValues.Length);
+            string reps = repValues[index].ToString();
 
             return reps;
         }
 
-        public static string SelectExcercises()
+        public static List<string> SelectExercises()
         {
-            string selectedExcercises = "Excercise 1, Excercise2";
+            Random random = new Random();
+            int noExercises = random.Next(2,5);
 
-            return selectedExcercises;
+            WorkoutData data = Workout.ReadData();
+            List<string> selectedExercises = new List<string>();
+
+            for (int i =0; i < noExercises; i++)
+            {
+                int index = random.Next(data.Exercises.Count);
+                selectedExercises.Add(data.Exercises[index].Name);
+            }
+
+            return selectedExercises;
         }
 
         public static string GenerateWorkout()
         {
-            string generatedWorkout = SelectWorkoutSchema();
+            Schema schema = SelectWorkoutSchema();
+            List<string> exercises = SelectExercises();
+            string timeLimit = schema.Time ? DetermineTimeLimit() : "";
+            string reps = schema.Reps ? DetermineReps() : "";
 
-            return generatedWorkout;
+            string workout = "Workout of the Day\n";
+                workout += $"{schema.Name}\n";
+                workout += $"{schema.Description}\n";
+                if (!string.IsNullOrEmpty(timeLimit))
+                    workout += $"Time: {timeLimit} minutes\n";
+                foreach (var exercise in exercises)
+                {
+                    if (!string.IsNullOrEmpty(reps))
+                        workout += $"- {reps} x {exercise}\n";
+                    else
+                        workout += $"- {exercise}\n";
+                }
+
+                return workout ;
+
         }
     }
 
@@ -65,36 +104,48 @@ namespace Program
     {
         public static WorkoutData ReadData()
         {
-            string json = File.ReadAllText("data.json");
-            return JsonSerializer.Deserialize<WorkoutData>(json);
+            string exePath = AppDomain.CurrentDomain.BaseDirectory;
+            string jsonPath = Path.Combine(exePath, "data.json");
+            string json = File.ReadAllText(jsonPath);
+
+            return JsonSerializer.Deserialize<WorkoutData>(json)!;
         }
     }
 
+    public class Email
+    {
+        public static void SendEmail()
+        {
+            Console.WriteLine("Send email to an address containing the Workout of the Day");
+        }
+    }
+
+
     public class WorkoutData
     {
-        public List<Exercise> Excercises { get; set; }
-        public List<Schema> Schemas { get; set; }
+        [JsonPropertyName("exercises")]
+        public required List<Exercise> Exercises { get; set; }
+        [JsonPropertyName("schemas")]
+        public required List<Schema> Schemas { get; set; }
     }
 
     public class Exercise
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
+        [JsonPropertyName("name")]
+        public required string Name { get; set; }
+        [JsonPropertyName("equipment")]
+        public required string Equipment { get; set; }
     }
 
     public class Schema
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public bool Time { get; set; }
-        public bool Reps { get; set; }
-    }
-
-    public class Email 
-    {
-        public static void SendEmail() 
-        {
-            Console.WriteLine("Send email to an address containing the Workout of the Day");
-        }
+        [JsonPropertyName("name")]
+        public required string Name { get; set; }
+        [JsonPropertyName("description")]
+        public required string Description { get; set; }
+        [JsonPropertyName("time")]
+        public required bool Time { get; set; }
+        [JsonPropertyName("reps")]
+        public required bool Reps { get; set; }
     }
 }
